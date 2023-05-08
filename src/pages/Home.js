bf767640bdfa727a6ad35d3597e5f1ab8d67c72d
Tatiana Chernova <fedoseeva.tv@gmail.com>
2023-05-08 15:03:15 +0300

    Changed some styles

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,11 +9,11 @@ const Home = () => {
                         <li className="content-list__item">
                             <h2 className="title-2">Frontend</h2>
                             <p>HTML5, CSS3, SCSS, SASS
-                                <br /> JavaScript, JQuery, Bootstrap, BEM, React</p>
+                                <br /> JavaScript, JQuery, Bootstrap, BEM, React, Gulp</p>
                         </li>
                         <li className="content-list__item">
                             <h2 className="title-2">Others</h2>
-                            <p>Git, GitHub, Figma, Gulp</p>
+                            <p>Git, GitHub, Figma</p>
                         </li>
                     </ul>
                 </div>
