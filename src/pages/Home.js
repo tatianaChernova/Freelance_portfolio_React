272cc0a473bae5f73a271c217eece01407b26770
Tatiana Chernova <fedoseeva.tv@gmail.com>
2023-07-20 14:32:40 +0300

    Made some changes at Home Page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,24 +2,29 @@ import Header from "./../components/header/Header";
 const Home = () => {
     return (
         <>
-            <Header />       
+            <Header />
             <main className="section">
                 <div className="container">
                     <ul className="content-list">
                         <li className="content-list__item">
-                            <h2 className="title-2">Frontend</h2>
-                            <p>HTML5, CSS3, SCSS, SASS
-                                <br /> JavaScript, JQuery, Bootstrap, BEM, React JS, Gulp</p>
+                            <h2 className="title-2">Education</h2>
+                            <p>Saint-Petersburg State University, Russia
+                                <br /> Applied Mathematics and Control Processes</p>
                         </li>
                         <li className="content-list__item">
-                            <h2 className="title-2">Others</h2>
+                            <h2 className="title-2">Frontend skills</h2>
+                            <p>HTML5, CSS3, SCSS, SASS
+                                <br /> JavaScript, JQuery, Bootstrap, BEM, Gulp, React JS </p>
+                        </li>
+                        <li className="content-list__item">
+                            <h2 className="title-2">Other skills</h2>
                             <p>Git, GitHub, Figma, Rest API</p>
                         </li>
                     </ul>
                 </div>
-            </main> 
+            </main>
         </>
-     );
+    );
 }
- 
+
 export default Home;
