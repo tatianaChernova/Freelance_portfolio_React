6da233a384c9abb1b8f2f9a08d5c8b192f43554b
Tatiana Chernova <fedoseeva.tv@gmail.com>
2023-09-15 09:01:50 +0300

    Changed links

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -11,7 +11,7 @@ const Header = () => {
                 <div className="header__text">
                     <p>with passion for learning and creating</p>
                 </div>
-                <a href="https://drive.google.com/file/d/1UxuD3zmKoQN_8szJajd5VEF-DFyFc3TI/view?usp=sharing" className="btn">Download CV</a>
+                <a href="https://drive.google.com/file/d/1UxuD3zmKoQN_8szJajd5VEF-DFyFc3TI/view?usp=drive_link" className="btn">Download CV</a>
             </div>
         </header>
      );
